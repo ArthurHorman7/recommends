f391b9b8fb75fd4ae05f86e548b762d819652d13
Arthur <arthur.yeshua@hotmail.com>
2023-07-10 16:07:28 -0300

    criando projeto

---
+++ src/main/java/com/api/recommends/RecommendsApplication.java
@@ -0,0 +1,13 @@
+package com.api.recommends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/recommends/RecommendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.recommends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
