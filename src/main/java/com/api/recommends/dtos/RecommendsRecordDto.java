05ccbecdd6edf428fa9c70f3f8ecbad702e21133
Arthur <arthur.yeshua@hotmail.com>
2023-07-11 10:03:26 -0300

    Criando o RecordDto

--- src/main/java/com/api/recommends/dtos/RecommendsRecordDto.java
+++ src/main/java/com/api/recommends/dtos/RecommendsRecordDto.java
@@ -1,2 +1,9 @@
-package com.api.recommends.dtos;public record RecommendsRecordDto() {
+package com.api.recommends.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record RecommendsRecordDto(@NotBlank String name, @NotBlank String author, @NotBlank String genre, @NotNull Integer year, @NotNull byte[] image, @NotBlank String description, @NotBlank BigDecimal rating) {
 }
