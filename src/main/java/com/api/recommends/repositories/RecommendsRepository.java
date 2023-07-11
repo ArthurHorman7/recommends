2234b6eca1bc248e2a9390eb8430778369979a94
Arthur <arthur.yeshua@hotmail.com>
2023-07-11 10:03:05 -0300

    Criando o Repository

--- src/main/java/com/api/recommends/repositories/RecommendsRepository.java
+++ src/main/java/com/api/recommends/repositories/RecommendsRepository.java
@@ -1,2 +1,11 @@
-package com.api.recommends.repositories;public interface RecommendsRepository {
+package com.api.recommends.repositories;
+
+import com.api.recommends.models.RecommendsModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RecommendsRepository extends JpaRepository<RecommendsModel, UUID> {
 }
