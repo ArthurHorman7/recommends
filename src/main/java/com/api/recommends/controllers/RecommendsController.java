bf0bc6548375cb2140ed222c3786cd2822cf46cd
Arthur <arthur.yeshua@hotmail.com>
2023-07-11 10:36:18 -0300

    criando o metodo getAll

--- src/main/java/com/api/recommends/controllers/RecommendsController.java
+++ src/main/java/com/api/recommends/controllers/RecommendsController.java
@@ -10,9 +10,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -26,6 +29,13 @@ public class RecommendsController {
         var recommendsModelValor = new RecommendsModel();
         BeanUtils.copyProperties(recommendsRecordDto, recommendsModelValor);
         return ResponseEntity.status(HttpStatus.CREATED).body(recommendsModelValor);
+    }
+
+    @GetMapping("/animes")
+    public ResponseEntity<List<RecommendsModel>> getAllRecommends(){
+        List<RecommendsModel> recommendsModelList = recommendsRepository.findAll();
+
+        return  ResponseEntity.status(HttpStatus.OK).body(recommendsModelList);
     }
 
 
